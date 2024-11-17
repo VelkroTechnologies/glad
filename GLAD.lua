9e66ca7a291a3f7d4a010e13fa78a30f8df05829
VelkroTechnologies <pjnanesh123@gmail.com>
2024-11-17 12:29:51 +1100

    Added GLAD files and premake support.

---
+++ GLAD.lua
@@ -0,0 +1,15 @@
+project "GLAD"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    warnings "off"
+    architecture "x64"
+
+    targetdir ("bin/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
+    objdir ("bin-int/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
+
+    files
+    {
+        "include/**.h",
+        "src/**.h"
+    }
